37a7f37af3168f3eb288ff02456d5e6332943179
Bonkomprann <waymaiker@laposte.net>
2023-05-11 13:43:25 +0200

    Fix warning <p> nested into <p>

--- frontend/components/FightView/components/FightAccess.jsx
+++ frontend/components/FightView/components/FightAccess.jsx
@@ -2,9 +2,11 @@ import { Text } from "@chakra-ui/react";
 
 export default function FightAccess(){
   return (
-    <Text fontSize={"xl"}>
-      You are ordering one ticket for 0.059 ether
-      <Text fontWeight={"bold"}>Continue ?</Text>
-    </Text>
+    <>
+      <Text fontSize={"xl"}>
+        You are ordering one ticket for 0.059 ether
+      </Text>
+      <Text fontSize={"xl"} fontWeight={"bold"}>Continue ?</Text>
+    </>
   )
 }
