b1a6c7804df173df8986baa26ce6ad7cb794f8d3
Bonkomprann <waymaiker@laposte.net>
2023-05-11 15:48:55 +0200

    Modify according to customModal modifications

--- frontend/components/EventsView/components/EventModalsCustomContent.jsx
+++ frontend/components/EventsView/components/EventModalsCustomContent.jsx
@@ -4,29 +4,33 @@ import CreateFightToken from "./CreateTokenFight"
 import CustomModal from "@/components/CustomModal/CustomModal"
 import FightAccess from "@/components/FightView/components/FightAccess"
 
-export const EventsModalsCustomContent = ({type, isOpen, onClose, customActionButton}) => {
+export const EventsModalsCustomContent = ({type, isOpen, onClose, getAccess, setWinner}) => {
   switch (type) {
     case "create token":
       return <CustomModal
         isOpen={isOpen}
         onClose={onClose}
+        title={"Create a token for this fight"}
         isCustomSize={true}
-        isCustomFooter={false}
+        showButtonsActionFooter={false}
         children={<CreateFightToken onClose={onClose} />}
       />
     case "getAccess":
       return <CustomModal
         isOpen={isOpen}
         onClose={onClose}
-        isCustomFooter={true}
-        customActionButton={customActionButton}
+        title={"Get access to this fight"}
+        mainButtonLabel={"VALIDATE"}
+        showButtonsActionFooter={true}
+        customActionButton={getAccess}
         children={<FightAccess/>}
       />
     case "ads":
       return <CustomModal
         isOpen={isOpen}
         onClose={onClose}
-        isCustomFooter={false}
+        title={"ADS"}
+        showButtonsActionFooter={true}
         mainButtonLabel={"SUBSCRIBE"}
         children={<Text fontSize={"xl"}> ADVERTISEMENT SPACE FOR OUR STREAMING PLATFORM </Text>}
       />
@@ -34,16 +38,19 @@ export const EventsModalsCustomContent = ({type, isOpen, onClose, customActionBu
       return <CustomModal
         isOpen={isOpen}
         onClose={onClose}
-        isCustomFooter={true}
+        title={"Information"}
+        showButtonsActionFooter={true}
         children={<Text fontSize={"xl"}> As a SuperAdmin you dont have access to this feature, contact an admin </Text>}
       />
-    case "connect":
+    case "setWinners":
       return <CustomModal
         isOpen={isOpen}
         onClose={onClose}
-        isCustomFooter={false}
+        title={"Who is the Winner ?"}
+        customActionButton={setWinner}
+        showButtonsActionFooter={true}
         mainButtonLabel={"OK"}
-        children={<Text fontSize={"xl"}> Come on! <Text>Connect and buy a ticket.</Text></Text>}
+        children={<Text fontSize={"xl"}> Come on, let's find out !  </Text>}
       />
     default:
       break;
