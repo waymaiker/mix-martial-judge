6006bfb6788c1cabb2acff5e9786d373fc9384b5
Bonkomprann <waymaiker@laposte.net>
2023-05-12 13:13:21 +0200

    Restructure component

--- frontend/components/EventsView/EventsView.jsx
+++ frontend/components/EventsView/EventsView.jsx
@@ -1,23 +1,75 @@
-import { Flex, Text } from '@chakra-ui/react';
-import { useEffect } from 'react';
 import { useAccount } from 'wagmi';
+import { Flex, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+import useWhoIsConnectedProvider from '@/hooks/useWhoIsConnectedProvider';
 
 import EventsContent from './components/EventsContent';
 
 export default function EventsView({events}){
+  //Wagmi
   const { isConnected, address } = useAccount()
 
-  useEffect(()=>{}, [isConnected, address])
+  //Providers
+  const { currentUser } = useWhoIsConnectedProvider()
+
+  //States
+  const [ selectedView, selectView ] = useState(false)
+  const [ listEvent, setListEvent ] = useState([])
+
+  useEffect(()=>{
+    setListEvent(events)
+  }, [isConnected, address])
+
+  useEffect(()=>{
+    selectedView
+    ? setListEvent(events.filter((event) => currentUser.finishedEvents.includes(parseInt(event.fightId))))
+    : setListEvent(events.filter((event) => !currentUser.finishedEvents.includes(parseInt(event.fightId))))
+  }, [selectedView])
 
   return (
     <>
+      <Flex justifyContent="center" mt="20" cursor={'default'}>
+        <Text
+          fontWeight="bold"
+          fontSize="7xl"
+          fontStyle="italic"
+          mr={"50px"}
+          onClick={()=>selectView(true)}
+          opacity={selectedView ? 1 : 0.5}
+        > PAST </Text>
+        <Text
+          fontWeight="bold"
+          fontSize="7xl"
+          fontStyle="italic"
+          onClick={()=>selectView(false)}
+          opacity={!selectedView ? 1 : 0.5}> UPCOMING </Text>
+      </Flex>
+      <Flex justifyContent="center" alignItems={'center'} mt="5">
+        {
+          listEvent.length == 0
+          ? <></>
+          : <Text fontWeight="bold" fontSize="3xl">
+              {listEvent.length} {listEvent.length > 1 ? "EVENTS" : "EVENT"}
+            </Text>
+        }
+      </Flex>
     {
-      events.length == 0
-      ? <Flex grow="1" justifyContent="center" alignItems="center" mt="5">
-          <Text fontWeight="bold" fontSize="7xl" fontStyle="italic"> NO UPCOMING EVENTS PROGRAMMED YET </Text>
+      listEvent.length != 0
+      ? <EventsContent
+          events={listEvent}
+          currentUser={currentUser}
+        />
+      : <Flex grow="1" justifyContent="center" alignItems="center" mt="5">
+        {
+          selectedView && listEvent.length == 0
+          ? <Text fontWeight="bold" fontSize="7xl" fontStyle="italic"> NO PAST EVENTS </Text>
+          : events.length == 0
+            ? <Text fontWeight="bold" fontSize="7xl" fontStyle="italic"> NO UPCOMING EVENTS PROGRAMMED YET </Text>
+            : <></>
+        }
         </Flex>
-      : <EventsContent events={events} />
-    }
+      }
     </>
   )
 }
--- frontend/components/EventsView/components/EventsContent.jsx
+++ frontend/components/EventsView/components/EventsContent.jsx
@@ -1,48 +1,12 @@
-import { useEffect, useState } from "react";
-import { Flex, Text } from "@chakra-ui/react";
-
 import { AnimationZoomListItem } from "@/utils/animations";
 
 import CardEvent from "./CardEvent";
-import useWhoIsConnectedProvider from "@/hooks/useWhoIsConnectedProvider";
 
-export default function EventsContent({events}){
-  //Providers
-  const { currentUser } = useWhoIsConnectedProvider()
-
-  //States
-  const [ selectedView, selectView ] = useState(true)
-  const [ listEvent, setListEvent ] = useState([])
-
-  useEffect(()=>{
-    selectedView
-    ? setListEvent(events.filter((event) => currentUser.finishedEvents.includes(parseInt(event.fightId))))
-    : setListEvent(events.filter((event) => !currentUser.finishedEvents.includes(parseInt(event.fightId))))
-  }, [selectedView])
-
+export default function EventsContent({events, currentUser}){
   return(
     <>
-      <Flex justifyContent="center" mt="20" cursor={'default'}>
-        <Text
-          fontWeight="bold"
-          fontSize="7xl"
-          fontStyle="italic"
-          mr={"50px"}
-          onClick={()=>selectView(true)}
-          opacity={selectedView ? 0.5 : 1}
-        > PAST </Text>
-        <Text
-          fontWeight="bold"
-          fontSize="7xl"
-          fontStyle="italic"
-          onClick={()=>selectView(false)}
-          opacity={!selectedView ? 0.5 : 1}> UPCOMING </Text>
-      </Flex>
-      <Flex justifyContent="center" mt="5">
-        <Text fontWeight="bold" fontSize="3xl"> {listEvent.length} {listEvent.length > 1 ? "EVENTS" : "EVENT"}  </Text>
-      </Flex>
       {
-        listEvent.map((event, index) =>
+        events.map((event, index) =>
           <AnimationZoomListItem key={index}>
             <CardEvent
               key={index}
