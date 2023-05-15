d43ed7faf56b100da04dac3650ae213c0dc76b7e
Bonkomprann <waymaiker@laposte.net>
2023-05-15 19:24:20 +0200

    Allow custom padding for a CardButton

--- frontend/components/EventsView/components/CardButton.jsx
+++ frontend/components/EventsView/components/CardButton.jsx
@@ -1,11 +1,12 @@
 import { Button, Text } from '@chakra-ui/react';
 
-export default function CardButton({title, action, secondaryAction, adminBackgroundColor, isDisabled, isLoading}){
+export default function CardButton({title, action, secondaryAction, adminBackgroundColor, isDisabled, isLoading, customPadding}){
   const onClickAction = action == undefined ? () => {} : () => action()
   const onClickAuxAction = secondaryAction == undefined ? () => {} : () => secondaryAction()
   const color = adminBackgroundColor == undefined ? "black" :"white"
   const colorScheme = adminBackgroundColor == undefined ? "" :"red"
   const backgroundColor = adminBackgroundColor == undefined ? "gray.200" : "red"
+  const padding = customPadding == undefined ? 7 : customPadding
 
   const customOnClick = () => {
     onClickAction(),
@@ -15,7 +16,7 @@ export default function CardButton({title, action, secondaryAction, adminBackgro
   return (
     <Button
       mt='1'
-      p={"7"}
+      p={padding}
       isDisabled={isDisabled}
       isLoading={isLoading}
       justifyContent="center"
