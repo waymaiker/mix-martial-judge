14906adbc48b6c373abb35f1dce51ea28ba535c5
Bonkomprann <waymaiker@laposte.net>
2023-05-15 19:26:25 +0200

    Quick wins

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>UFC</title>
+        <title>MMJ</title>
         <meta name="description" content="pet project on UFC" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
